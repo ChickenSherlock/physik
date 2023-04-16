e521b75fdbddf22a41dcd5d54c81d4e8bb93df6c
ChickenSherlock <benedikt.karli@gmail.com>
2023-04-16 17:01:47 +0200

    added Readme

---
+++ README.md
@@ -0,0 +1,4 @@
+<h1>The better Photo booth written in Rust</h1>
+<p>ps. not used npm once KEKW</p>
+<p>pps. even runs on Raspberry PI 4</p>
+[![Rust](https://github.com/ChickenSherlock/physik/actions/workflows/rust.yml/badge.svg)](https://github.com/ChickenSherlock/physik/actions/workflows/rust.yml)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use gtk::{Application};
+
+const APP_ID: &str = "BETTER_PHOTO_BOOTH";
+const CAMERA_WIDTH: i64 = 1920;
+const CAMERA_HEIGHT: i64 = 1080;
+const ROW_STRIDE: i64 = CAMERA_WIDTH * 3;
+
+fn read_camera() {
+
+}
+
+fn draw_ui() {
+
+}
+
+
+fn main(){
+    let app = Application::builder()
+        .application_id(APP_ID)
+        .build();
 }
