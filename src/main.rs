ec07da2cdd7b874fcc9c7ea3c0ffc7dd57858313
ChickenSherlock <benedikt.karli@gmail.com>
2023-04-27 12:08:45 +0200

    finished

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,6 @@ use serde_json;
 use opencv::{
     Result,
     prelude::*,
-    objdetect,
     imgproc::cvt_color,
     imgcodecs,
     types,
@@ -56,23 +55,11 @@ fn create_camera()->Result<(videoio::VideoCapture,Mat)>{
 fn camera_stream(sender: glib::Sender<((Vec<u8>))>, cambool: Arc<AtomicBool>, uuid_sender: glib::Sender<String>, authbool_clone: Arc<AtomicBool>){
     thread::spawn(move||{
         let (mut camera, mut mat) =if let Ok((mut camera, mut mat)) =create_camera() { (camera, mat) } else {todo!("")};
-        let mut qr_detector = objdetect::QRCodeDetector::default().expect("detector failed");
-        let mut qr_result = opencv::types::VectorOfPoint::new();
-        let mut qr_mat = Mat::default();
-        let mut timer = Instant::now();
         loop {
             if !cambool.load(Ordering::Relaxed){
                 // reading camera footage
                 camera.read(&mut mat).expect("Camera not available");
                 // calling decode function every second
-                if timer.elapsed() > Duration::from_secs(1) && authbool_clone.load(Ordering::Relaxed) == false{
-                    let ret = qr_detector.detect_and_decode(&mat,&mut qr_result, &mut qr_mat).unwrap();
-                    if !ret.is_empty(){
-                        // Checking if the qr code is valid if so send to main thread
-                        uuid_sender.send(String::from_utf8(ret).unwrap());
-                    }
-                    timer = Instant::now();
-                }
                 // some basic image manipulation
                 let mut image = Mat::default();
                 cvt_color(&mat, &mut image, opencv::imgproc::COLOR_BGR2RGB, 0).unwrap();
@@ -109,11 +96,13 @@ fn draw_ui(app: &Application) {
     // main atomic bool that shuts down camera in camera_stream thread allows other threads to access camera
     let cambool = Arc::new(AtomicBool::new(false));
     let cambool_1 = cambool.clone();
-    // atomic bool to if user has been authenticated
+    let cambool_5 = cambool.clone();
 
     let authbool = Arc::new(AtomicBool::new(false));
     let authbool_clone = authbool.clone();
     let abc = authbool.clone();
+    let abc_ =authbool.clone();
+    let au = authbool.clone();
 
 
     let stream = Image::builder()
@@ -129,10 +118,21 @@ fn draw_ui(app: &Application) {
         .row_spacing(600)
         .build();
 
+    let button_1 = Button::builder()
+        .label("Herr Detlefsen")
+        .build();
+
+    let button_2 = Button::builder()
+        .label("Beni")
+        .build();
+
+
+
+
 
 
     let overlay_label = Label::builder()
-        .label("Scanne deinen QR Code")
+        .label("")
         .name("label")
         .build();
 
@@ -143,6 +143,9 @@ fn draw_ui(app: &Application) {
         .child(&stream)
         .build();
 
+    let overlay_clone = overlay.clone();
+    grid.attach(&button_2,1,1,1,1);
+    grid.attach(&button_1,0,1,1,1);
     grid.attach(&overlay_label,0,0,1,1);
 
 
@@ -160,6 +163,7 @@ fn draw_ui(app: &Application) {
 
     let (finished_sender,finished_receiver) = glib::MainContext::channel(Priority::HIGH);
     // main channel that allows uuids to be passed to main ui thread
+    let (finished_sender_,finished_receiver_) = glib::MainContext::channel(Priority::HIGH);
 
     let (uuid_sender,uuid_receiver) = glib::MainContext::channel(glib::Priority::HIGH);
 
@@ -171,58 +175,105 @@ fn draw_ui(app: &Application) {
 
     main_box.attach(&overlay,0,1,1,1);
     let main_box_clone = main_box.clone();
+    let main_box_clone_ = main_box.clone();
     let o_label_clone = overlay_label.clone();
 
-    uuid_receiver.attach(None,move|data|{
+    button_1.connect_button_press_event(move|_,_|{
+        let uuid = "Herr-Detlefsen";
         let ol_clone = o_label_clone.clone();
         let u = ol_clone.clone();
         let cambool_3 = cambool.clone();
         let person_sender = finished_sender.clone();
         let auth_list = get_access_information();
-        for person in auth_list.access{
-            if person.uuid.to_string() == data{
-                authbool.store(true,Ordering::Relaxed);
-                ol_clone.set_label("QR Code gescannt Fotos werden in 3 sekunden gemacht");
-                glib::timeout_add_local_once(Duration::from_secs(4),move||{
-                    let l = ol_clone.clone();
-                    l.set_label("Jetzt gehts los");
-                });
 
-                glib::timeout_add_local_once(Duration::from_secs(5),move||{
-                    let a = u.clone();
-                    a.set_label("");
-                });
-                let thread = thread::spawn(move||{
-                    thread::sleep(Duration::from_secs(7));
-                    let finished_sender_clone = person_sender.clone();
-                    for i in 0..3{
-                        let cambool_2 = cambool_3.clone();
-                        cambool_2.store(true,Ordering::Relaxed);
-                        let mut cam = videoio::VideoCapture::new(0, videoio::CAP_ANY).expect("Error");
-                        let mut mat = Mat::default();
-                        cam.read(&mut mat).expect("Camera not available");
-                        cambool_2.store(false,Ordering::Relaxed);
-                        let mut dest = Mat::default();
-                        let empty_array:opencv::core::Vector<i32> = opencv::core::Vector::new();
-                        core::flip(&mat, &mut dest, 1).unwrap();
-                        imwrite(&format!("/Users/benediktkarli/Desktop/physik/v2/src/{:?}-{:?}.jpg",person.uuid.to_owned(),i).to_owned(),&dest,&empty_array);
-                        thread::sleep(Duration::from_secs(1))
-                    }
-                    let uuid = person.uuid.clone();
-                    finished_sender_clone.send(uuid)
-                });
-                break
 
+        au.store(true,Ordering::Relaxed);
+        ol_clone.set_label("Fotos werden in 3 sekunden gemacht");
+        glib::timeout_add_local_once(Duration::from_secs(4),move||{
+            let l = ol_clone.clone();
+            l.set_label("Jetzt gehts los");
+        });
+
+        glib::timeout_add_local_once(Duration::from_secs(5),move||{
+            let a = u.clone();
+            a.set_label("");
+        });
+        let thread = thread::spawn(move||{
+            thread::sleep(Duration::from_secs(7));
+            let finished_sender_clone = person_sender.clone();
+            for i in 0..3{
+                let cambool_2 = cambool_3.clone();
+                cambool_2.store(true,Ordering::Relaxed);
+                let mut cam = videoio::VideoCapture::new(0, videoio::CAP_ANY).expect("Error");
+                let mut mat = Mat::default();
+                cam.read(&mut mat).expect("Camera not available");
+                cambool_2.store(false,Ordering::Relaxed);
+                let mut dest = Mat::default();
+                let empty_array:opencv::core::Vector<i32> = opencv::core::Vector::new();
+                core::flip(&mat, &mut dest, 1).unwrap();
+                imwrite(&format!("/Users/benediktkarli/Desktop/physik/v2/src/{:?}-{:?}.jpg",uuid.to_owned(),i).to_owned(),&dest,&empty_array);
+                thread::sleep(Duration::from_secs(1))
             }
-        }
-        glib::Continue(true)
+            finished_sender_clone.send(uuid)
+        });
+        Inhibit(true)
     });
+
+    let u = overlay_label.clone();
+
+    button_2.connect_button_press_event(move|_,_|{
+        let uuid = "Benedikt";
+        let ol_clone = u.clone();
+        let u = ol_clone.clone();
+        let cambool_3 = cambool_5.clone();
+        let person_sender = finished_sender_.clone();
+        let auth_list = get_access_information();
+
+
+        authbool.store(true,Ordering::Relaxed);
+        ol_clone.set_label("Fotos werden in 3 sekunden gemacht");
+        glib::timeout_add_local_once(Duration::from_secs(4),move||{
+            let l = ol_clone.clone();
+            l.set_label("Jetzt gehts los");
+        });
+
+        glib::timeout_add_local_once(Duration::from_secs(5),move||{
+            let a = u.clone();
+            a.set_label("");
+        });
+        let thread = thread::spawn(move||{
+            thread::sleep(Duration::from_secs(7));
+            let finished_sender_clone = person_sender.clone();
+            for i in 0..3{
+                let cambool_2 = cambool_3.clone();
+                cambool_2.store(true,Ordering::Relaxed);
+                let mut cam = videoio::VideoCapture::new(0, videoio::CAP_ANY).expect("Error");
+                let mut mat = Mat::default();
+                cam.read(&mut mat).expect("Camera not available");
+                cambool_2.store(false,Ordering::Relaxed);
+                let mut dest = Mat::default();
+                let empty_array:opencv::core::Vector<i32> = opencv::core::Vector::new();
+                core::flip(&mat, &mut dest, 1).unwrap();
+                imwrite(&format!("/Users/benediktkarli/Desktop/physik/v2/src/{:?}-{:?}.jpg",uuid.to_owned(),i).to_owned(),&dest,&empty_array);
+                thread::sleep(Duration::from_secs(1))
+            }
+            finished_sender_clone.send(uuid)
+        });
+        Inhibit(true)
+    });
+
+
+
     let mb = main_box.clone();
     let cg = choose_grid.clone();
     let o = overlay.clone();
     let ol = overlay_label.clone();
 
-    finished_receiver.attach(None,move|uuid: i32|{
+    let mb_ = main_box.clone();
+    let cg_ = choose_grid.clone();
+    let o_ = overlay.clone();
+    let ol_ = overlay_label.clone();
+    finished_receiver_.attach(None,move|uuid: &str|{
         for i in 0..3{
             let t = ol.clone();
             let u = mb.clone();
@@ -255,9 +306,49 @@ fn draw_ui(app: &Application) {
             choose_grid.attach(&image,i,0,1,1);
             choose_grid.attach(&button,i,1,1,1);
         }
-        main_box_clone.remove(&overlay);
+        main_box_clone.remove(&overlay_clone);
         choose_grid.show_all();
         main_box_clone.attach(&choose_grid,0,0,1,1);
+
+        glib::Continue(true)
+    });
+
+    finished_receiver.attach(None,move|uuid: &str|{
+        for i in 0..3{
+            let t = ol_.clone();
+            let u = mb_.clone();
+            let ui = abc_.clone();
+            let e = cg_.clone();
+            let z = o_.clone();
+            let image = Image::builder()
+                .pixbuf(&Pixbuf::from_file_at_scale(<std::path::Path as AsRef<Path>>::as_ref(Path::new(&format!("/Users/benediktkarli/Desktop/physik/v2/src/{:?}-{:?}.jpg",uuid,i)).as_ref()),426,239,false).unwrap())
+                .vexpand(true)
+                .hexpand(true)
+                .build();
+
+            let button = Button::builder()
+                .label(format!("Bild: {:?}",i + 1))
+                .margin_bottom(100)
+                .build();
+            button.set_widget_name(i.to_string().borrow());
+
+            button.connect_button_press_event(move|a,event|{
+                std::fs::copy(<std::path::Path as AsRef<Path>>::as_ref(Path::new(&format!("/Users/benediktkarli/Desktop/physik/v2/src/{:?}-{:?}.jpg",uuid,i)).as_ref()),<std::path::Path as AsRef<Path>>::as_ref(Path::new(&format!("/Users/benediktkarli/Desktop/physik/v2/src/output/{:?}.jpg",uuid)).as_ref()));
+                let i = u.clone();
+                let x = ui.clone();
+                let y = t.clone();
+                y.set_label("Scanne deinen QR Code");
+                i.remove(&e);
+                i.attach(&z,0,0,1,1);
+                x.store(false,Ordering::Relaxed);
+                Inhibit(true)
+            });
+            cg_.attach(&image,i,0,1,1);
+            cg_.attach(&button,i,1,1,1);
+        }
+        main_box_clone_.remove(&overlay);
+        cg_.show_all();
+        main_box_clone_.attach(&cg_,0,0,1,1);
 
         glib::Continue(true)
     });
